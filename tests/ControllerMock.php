5a71617f46b90e93946beb860efb7f11d19503c9
Natan Felles <natanfelles@gmail.com>
2019-09-12 18:53:35 -0300

    Update Request function signature on mocks

--- tests/ControllerMock.php
+++ tests/ControllerMock.php
@@ -9,16 +9,26 @@ class ControllerMock extends Controller
 	public function __construct()
 	{
 		$request = new class() extends Request {
-			protected function filterInput(int $type) : array
-			{
+			protected function filterInput(
+				int $type,
+				string $variable = null,
+				int $filter = null,
+				$options = null
+			) {
+				$data = [];
 				if ($type === \INPUT_SERVER) {
-					return [
+					$data = [
 						'HTTP_HOST' => 'localhost',
 						'REQUEST_METHOD' => 'GET',
 						'SERVER_PROTOCOL' => 'HTTP/1.1',
 					];
 				}
-				return [];
+				$variable = $variable === null
+					? $data
+					: \ArraySimple::value($variable, $data);
+				return $filter
+					? \filter_var($variable, $filter, $options)
+					: $variable;
 			}
 		};
 		parent::__construct($request, new Response($request));
