8d03bc66225ba34cca2c4b48b5f356f998ec8ec8
Natan Felles <natanfelles@gmail.com>
2019-10-01 10:33:17 -0300

    Add tests for App

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -2,6 +2,7 @@
 
 use Framework\Autoload\Autoloader;
 use Framework\Autoload\Locator;
+use Framework\Cache\Cache;
 use Framework\CLI\Console;
 use Framework\Database\Database;
 use Framework\Email\Mailer;
@@ -64,17 +65,56 @@ class AppTest extends TestCase
 	 */
 	public function testServicesInstances()
 	{
+		App::setConfig('cache', [
+			'driver' => 'Files',
+			'configs' => [
+				'directory' => '/tmp',
+				'length' => 4096,
+			],
+			'prefix' => null,
+			'serializer' => 'php',
+		]);
 		$this->assertInstanceOf(Autoloader::class, App::getAutoloader());
+		$this->assertInstanceOf(Autoloader::class, App::getAutoloader());
+		$this->assertInstanceOf(Cache::class, App::getCache());
+		$this->assertInstanceOf(Cache::class, App::getCache());
+		$this->assertInstanceOf(Console::class, App::getConsole());
 		$this->assertInstanceOf(Console::class, App::getConsole());
 		$this->assertInstanceOf(Database::class, App::getDatabase());
+		$this->assertInstanceOf(Database::class, App::getDatabase());
+		$this->assertInstanceOf(Language::class, App::getLanguage());
 		$this->assertInstanceOf(Language::class, App::getLanguage());
 		$this->assertInstanceOf(Locator::class, App::getLocator());
+		$this->assertInstanceOf(Locator::class, App::getLocator());
+		$this->assertInstanceOf(Mailer::class, App::getMailer());
 		$this->assertInstanceOf(Mailer::class, App::getMailer());
 		//$this->assertInstanceOf(Request::class, App::getRequest());
 		//$this->assertInstanceOf(Response::class, App::getResponse());
 		$this->assertInstanceOf(Router::class, App::getRouter());
+		$this->assertInstanceOf(Router::class, App::getRouter());
+		$this->assertInstanceOf(Session::class, App::getSession());
 		$this->assertInstanceOf(Session::class, App::getSession());
 		$this->assertInstanceOf(Validation::class, App::getValidation());
+		$this->assertInstanceOf(Validation::class, App::getValidation());
 		$this->assertInstanceOf(View::class, App::getView());
+		$this->assertInstanceOf(View::class, App::getView());
+	}
+
+	public function testAutoloaderWithConfigs()
+	{
+		App::setConfig('autoloader', [
+			'namespaces' => [
+				__NAMESPACE__ => __DIR__,
+			],
+			'classes' => [
+				__CLASS__ => __FILE__,
+			],
+		]);
+		$this->assertEquals([
+			__NAMESPACE__ => __DIR__ . '/',
+		], App::getAutoloader()->getNamespaces());
+		$this->assertEquals([
+			__CLASS__ => __FILE__,
+		], App::getAutoloader()->getClasses());
 	}
 }
