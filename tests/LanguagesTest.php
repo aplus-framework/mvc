93d5e7b6238a0443c097e2887bd2bc23927c2fc6
Natan Felles <natanfelles@gmail.com>
2019-12-10 17:44:29 -0300

    Test Language lines

---
+++ tests/LanguagesTest.php
@@ -0,0 +1,29 @@
+<?php namespace Tests\MVC;
+
+use PHPUnit\Framework\TestCase;
+
+class LanguagesTest extends TestCase
+{
+	protected $langDir = __DIR__ . '/../src/Languages/';
+
+	protected function getCodes()
+	{
+		$codes = \array_filter(\glob($this->langDir . '*'), 'is_dir');
+		$length = \strlen($this->langDir);
+		foreach ($codes as &$dir) {
+			$dir = \substr($dir, $length);
+		}
+		return $codes;
+	}
+
+	public function testKeys()
+	{
+		$rules = ['inDatabase', 'notInDatabase'];
+		foreach ($this->getCodes() as $code) {
+			$lines = require $this->langDir . $code . '/validation.php';
+			$lines = \array_keys($lines);
+			\sort($lines);
+			$this->assertEquals($rules, $lines, 'Language: ' . $code);
+		}
+	}
+}
