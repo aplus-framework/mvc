3acc9ffa486a7fcc0864821bd8d855b1a3a5caf7
Natan Felles <natanfelles@gmail.com>
2021-05-28 17:05:02 -0300

    Add PHPDocs on helpers

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,5 @@
 <?php
 
-use Framework\HTTP\Response;
 use Framework\MVC\App;
 use JetBrains\PhpStorm\Pure;
 
@@ -47,6 +46,13 @@ if ( ! function_exists('esc')) {
 	}
 }
 if ( ! function_exists('normalize_whitespaces')) {
+	/**
+	 * Normalize string whitespaces.
+	 *
+	 * @param string $string
+	 *
+	 * @return string
+	 */
 	#[Pure]
 	function normalize_whitespaces(string $string) : string
 	{
@@ -80,18 +86,37 @@ if ( ! function_exists('view')) {
 	}
 }
 if ( ! function_exists('current_url')) {
+	/**
+	 * Get the current URL.
+	 *
+	 * @return string
+	 */
 	function current_url() : string
 	{
 		return App::request()->getURL();
 	}
 }
 if ( ! function_exists('current_route')) {
+	/**
+	 * Get the current Route.
+	 *
+	 * @return \Framework\Routing\Route
+	 */
 	function current_route() : Framework\Routing\Route
 	{
 		return App::router()->getMatchedRoute();
 	}
 }
 if ( ! function_exists('route_url')) {
+	/**
+	 * Get an URL based in a Route name.
+	 *
+	 * @param string $name          Route name
+	 * @param array  $path_params   Route path parameters
+	 * @param array  $origin_params Route origin parameters
+	 *
+	 * @return string The URL
+	 */
 	function route_url(string $name, array $path_params = [], array $origin_params = []) : string
 	{
 		$route = App::router()->getNamedRoute($name);
@@ -106,11 +131,15 @@ if ( ! function_exists('route_url')) {
 }
 if ( ! function_exists('lang')) {
 	/**
-	 * @param string         $line
-	 * @param array|string[] $args
-	 * @param string|null    $locale
+	 * Renders a language file line with dot notation format.
 	 *
-	 * @return string|null
+	 * e.g. home.hello matches home for file and hello for line.
+	 *
+	 * @param string         $line   The dot notation file line
+	 * @param array|string[] $args   The arguments to be used in the formatted text
+	 * @param string|null    $locale A custom locale or null to use the current
+	 *
+	 * @return string|null The rendered text or null if not found
 	 */
 	function lang(string $line, array $args = [], string $locale = null) : ?string
 	{
@@ -118,18 +147,42 @@ if ( ! function_exists('lang')) {
 	}
 }
 if ( ! function_exists('cache')) {
+	/**
+	 * Get a Cache instance.
+	 *
+	 * @param string $instance
+	 *
+	 * @return \Framework\Cache\Cache
+	 */
 	function cache(string $instance = 'default') : Framework\Cache\Cache
 	{
 		return App::cache($instance);
 	}
 }
 if ( ! function_exists('session')) {
+	/**
+	 * Get the Session instance.
+	 *
+	 * @return \Framework\Session\Session
+	 */
 	function session() : Framework\Session\Session
 	{
 		return App::session();
 	}
 }
 if ( ! function_exists('old')) {
+	/**
+	 * Get data from old redirect.
+	 *
+	 * @param string|null $key    Set null to return all data
+	 * @param bool        $escape
+	 *
+	 * @see \Framework\HTTP\Request::getRedirectData
+	 * @see \Framework\HTTP\Response::redirect
+	 *
+	 * @return mixed The old value. If $escape is true and the value is not
+	 *               stringable, an empty string will return
+	 */
 	function old(?string $key, bool $escape = true) : mixed
 	{
 		session();
@@ -159,7 +212,7 @@ if ( ! function_exists('not_found')) {
 	 *
 	 * @return \Framework\HTTP\Response
 	 */
-	function not_found(array $data = []) : Response
+	function not_found(array $data = []) : Framework\HTTP\Response
 	{
 		App::response()->setStatusLine(404);
 		if (App::request()->isJSON()) {
