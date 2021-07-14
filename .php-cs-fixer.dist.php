a6345fd0441b51d655d55685eafb547cb0fc2d11
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:00:34 -0300

    Set new default header comment

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,4 +12,4 @@ use Framework\CodingStandard\Finder;
 
 return (new Config())->setFinder(
     Finder::create()->in(__DIR__)
-)->setDefaultHeaderComment('The Framework MVC Library');
+)->setDefaultHeaderComment('Aplus Framework MVC Library');
