320d47b824b3e47c7534e220c5a19d6b7c440dbe
Natan Felles <natanfelles@gmail.com>
2022-08-05 18:38:51 -0300

    Override return classes for App::getService method

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -70,3 +70,23 @@ expectedArguments(
     0,
     argumentsSet('service_names')
 );
+override(\Framework\MVC\App::getService(), map([
+    'antiCsrf' => \Framework\HTTP\AntiCSRF::class,
+    'autoloader' => \Framework\Autoload\Autoloader::class,
+    'cache' => \Framework\Cache\Cache::class,
+    'console' => \Framework\CLI\Console::class,
+    'database' => \Framework\Database\Database::class,
+    'debugger' => \Framework\Debug\Debugger::class,
+    'exceptionHandler' => \Framework\Debug\ExceptionHandler::class,
+    'language' => \Framework\Language\Language::class,
+    'locator' => \Framework\Autoload\Locator::class,
+    'logger' => \Framework\Log\Logger::class,
+    'mailer' => \Framework\Email\Mailer::class,
+    'migrator' => \Framework\Database\Extra\Migrator::class,
+    'request' => \Framework\HTTP\Request::class,
+    'response' => \Framework\HTTP\Response::class,
+    'router' => \Framework\Routing\Router::class,
+    'session' => \Framework\Session\Session::class,
+    'validation' => \Framework\Validation\Validation::class,
+    'view' => \Framework\MVC\View::class,
+]));
