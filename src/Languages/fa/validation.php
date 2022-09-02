7b1f46541d887a72e397430b0cad25490e02e6f9
Mehran-tr <mehran.taheri.t@email.com>
2022-09-02 15:49:31 +0430

    add persian language

---
+++ src/Languages/fa/validation.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * This file is part of Aplus Framework MVC Library.
+ *
+ * (c) Mehran Taheri <mehran.taheri.t@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+return [
+    'notUnique' => 'فیلد {field} ثبت نشده است.',
+    'unique' => 'فیلد {field} قبلا ثبت شده است.',
+];
